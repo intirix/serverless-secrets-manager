eaf79eb4fd7c6e16155341e7382323a4bbcc3c29
Jeff Mercer <jeff.mercer@gmail.com>
2017-06-23 10:59:28 -0400

    Added create-secret and get-secret CLI calls

--- cli.py
+++ cli.py
@@ -6,16 +6,21 @@ import client
 import system
 import db
 import logging
+import getpass
+import crypto
+import json
 
 class CLI:
 
 	def __init__(self,args):
 		self.args = args
+		self.crypto = crypto.Crypto()
 
 	def help(self):
 		print(sys.argv[0]+" <flags> <command> <arguments>")
 		print("Flags:")
 		print("  -d - direct mode - Directly contact database instead of going through REST API")
+		print("  -u [username] - user to log in a")
 		print("")
 		print("Commands:")
 		print("  list-users")
@@ -27,16 +32,25 @@ class CLI:
 		print("      displayName - what get's displayed to users")
 		print("        optional - defaults to username")
 		print("")
+		print("  create-secret")
+		print("    Create a new (empty) secret.  You can add extra fields later")
+		print("")
+		print("  get-secret <secretID>")
+		print("    Get a secret (requires a private key)")
+		print("")
 
 	def parse(self):
 
-		optlist, self.args = getopt.getopt(self.args, 'd')
+		optlist, self.args = getopt.getopt(self.args, 'du:')
 
 		self.mode = "rest"
+		self.user = "admin"
 
 		for o, a in optlist:
 			if o=='-d':
 				self.mode = "direct"
+			elif o=='-u':
+				self.user = a
 			else:
 				assert False, "unhandled option"
 
@@ -56,11 +70,18 @@ class CLI:
 		self.system.init()
 
 	def login(self):
+		print("Logging in as user: "+self.user)
 		if self.mode == 'direct':
 			# direct access doesn't require a password
-			self.client.login("admin","")
+			self.client.login(self.user,"")
+			self.password = None
 		else:
 			raise(Exception("Mode ["+self.mode+"] not implemented yet"))
+
+	def getPassword(self):
+		if self.password == None:
+			self.password = getpass.getpass("Password:")
+		return self.password
 
 	def run(self):
 
@@ -82,6 +103,55 @@ class CLI:
 				display = self.args[1]
 
 			self.client.addUser(user,display)
+		elif command == "create-secret":
+
+			pubKey = self.client.getUsersPublicKey(self.user)
+			aesKey = self.crypto.generateRandomKey()
+			hmacKey = self.crypto.generateRandomKey()
+
+			# I don't want to just encrypt {}, I want some randomness in there
+			rnd = self.crypto.encode(self.crypto.generateRandomKey())
+
+			# Encrypt an empty secret for now
+			encryptedSecret = self.crypto.encrypt(aesKey,json.dumps({"random":rnd}))
+			encryptedKey = self.crypto.encryptRSA(pubKey,aesKey)
+
+			hmac = self.crypto.createHmac(hmacKey,encryptedSecret)
+
+			eek = self.crypto.encode(encryptedKey)
+			ehk = self.crypto.encode(hmacKey)
+
+			sid = self.client.addSecret(self.user,"1",eek,ehk,encryptedSecret,hmac)
+
+			print("Secret ID: "+str(sid))
+
+		elif command == "get-secret":
+			if len(self.args)==0:
+				self.help()
+				raise Exception("Expected argument <secretID>")
+
+			sid = self.args[0]
+
+			privKey = self.client.getUserPrivateKey(self.user,self.getPassword())
+
+			secretEntry = self.client.getSecret(sid)
+
+
+			encryptedKey = self.crypto.decode(secretEntry["users"][self.user]["encryptedKey"])
+			hmacKey = self.crypto.decode(secretEntry["hmacKey"])
+			storedHmac = secretEntry["hmac"]
+			storedEncryptedSecret = secretEntry["encryptedSecret"]
+
+			if not self.crypto.verifyHmac(hmacKey,storedEncryptedSecret,storedHmac):
+				raise(Exception("Secret verification failed!"))
+
+			origKey = self.crypto.decryptRSA(privKey,encryptedKey)
+			origSecretText = self.crypto.decrypt(origKey,storedEncryptedSecret)
+			origSecret = json.loads(origSecretText)
+			del origSecret["random"]
+
+			print(json.dumps(origSecret,indent=2))
+			
 		else:
 			self.help()
 			raise Exception("Unknown command: "+command)
--- client.py
+++ client.py
@@ -16,10 +16,22 @@ class ClientSystemInterface:
 	def addUser(self,user,display):
 		return self.system.addUser(user,display)
 
+	def getUserPrivateKey(self,user,password):
+		return self.system.getUserPrivateKey(user,password)
+
 	def canCreateUser(self,user):
 		# Direct access always can create users
 		return True
 
+	def canUserReadSecret(self,user,sid):
+		entry = self.system.getSecret(sid)
+		return user in entry["users"]
+
+	def addSecret(self,owner,secretEncryptionProfile,encryptedKey,hmacKey,encryptedSecret,hmac):
+		return self.system.addSecret(owner,secretEncryptionProfile,encryptedKey,hmacKey,encryptedSecret,hmac)
+
+	def getSecret(self,sid):
+		return self.system.getSecret(sid)
 
 class Client:
 
@@ -33,8 +45,22 @@ class Client:
 	def listUsers(self):
 		return self.iface.listUsers()
 
+	def getUsersPublicKey(self,user):
+		return self.listUsers()[user]["publicKey"]
+
+	def getUserPrivateKey(self,user,password):
+		return self.iface.getUserPrivateKey(user,password)
+
 	def addUser(self,user,display):
 		if self.iface.canCreateUser(self.username):
 			return self.iface.addUser(user,display)
 
+	def addSecret(self,owner,secretEncryptionProfile,encryptedKey,hmacKey,encryptedSecret,hmac):
+		return self.iface.addSecret(owner,secretEncryptionProfile,encryptedKey,hmacKey,encryptedSecret,hmac)
+
+	def getSecret(self,sid):
+		if self.iface.canUserReadSecret(self.username,sid):
+			return self.iface.getSecret(sid)
+		else:
+			raise(Exception("Access denied"))
 
