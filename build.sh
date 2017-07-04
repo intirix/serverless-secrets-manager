ed688d9104fb9bc87eaaab6f37f2fee1561511b5
Jeff Mercer <jeff.mercer@gmail.com>
2017-07-03 20:28:02 -0400

    Added code coverage

--- build.sh
+++ build.sh
@@ -8,11 +8,13 @@ workspace="$( dirname $0 )"
 	cd "$workspace"
 	workspace="$(pwd)"
 
-
+	.contents/bin/coverage erase
 	for x in *_test.py
 	do
-		.contents/bin/python $x
+		#.contents/bin/coverage run -a --source $( echo *.py | tr ' ' ',' ) $x
+		.contents/bin/coverage run -a --omit=.contents/*,*_test.py $x
 	done
+	.contents/bin/coverage report
 
 	echo zip lambda.zip *.py
 	zip lambda.zip *.py
